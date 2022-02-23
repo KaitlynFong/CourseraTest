bce06007c8ca9254f6d02337231160876012c111
KaitlynFong <62269737+KaitlynFong@users.noreply.github.com>
2022-02-23 12:05:12 -0800

    Update script.js

--- Module4_solutions/script.js
+++ Module4_solutions/script.js
@@ -1,50 +1,10 @@
-// *******************************
-// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
-// *******************************
-//
-// Module 4 Assignment Instructions.
-//
-// The idea of this assignment is to take an existing array of names
-// and then output either Hello 'Name' or Good Bye 'Name' to the console.
-// The program should say "Hello" to any name except names that start with a "J"
-// or "j", otherwise, the program should say "Good Bye". So, the final output
-// on the console should look like this:
-/*
-Hello Yaakov
-Good Bye John
-Good Bye Jen
-Good Bye Jason
-Hello Paul
-Hello Frank
-Hello Larry
-Hello Paula
-Hello Laura
-Good Bye Jim
-
-WARNING!!! WARNING!!!
-The code does NOT currently work! It is YOUR job to make it work
-as described in the requirements and the steps in order to complete this
-assignment.
-WARNING!!! WARNING!!!
-
-*/
-
-// STEP 1:
-// Wrap the entire contents of script.js inside of an IIFE
 (function (window){
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-  // STEP 10:
-  // Loop over the names array and say either 'Hello' or "Good Bye"
+  
   for (var i = 0; i < names.length; i++) {
-
-    // STEP 11:
-    // Retrieve the first letter of the current name in the loop.
-    var firstLetter = names[i].charAt(0);
     
-    // STEP 12:
-    // If the firstLetter = j, call byeSpeaker's 'speak' method 
-    //  Otherwise, call helloSpeaker's 'speak' method 
+    var firstLetter = names[i].charAt(0);
+
     if (firstLetter.toLowerCase() == "j"){
       byeSpeaker.speak(names[i]);
     } else{
